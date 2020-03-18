4c2e52be9235c74c9ba64d843636959a1e40891b
Allan Corbett <allancorbett@gmail.com>
2020-03-18 10:28:04 +0000

    Add video

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script>
+  let clip = "example.mp4";
   import Heading from "../components/Heading.svelte";
   import Text from "../components/Text.svelte";
   import Grid from "../components/Grid.svelte";
@@ -7,6 +8,18 @@
 <style>
   .title {
     border-bottom: var(--border-width-hefty) solid var(--color-body-text);
+  }
+  .render {
+    background: var(--render-background);
+    padding: var(--space-5);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .video {
+    max-width: 100%;
+    margin: 0;
+    box-shadow: var(--shadow-0);
   }
 </style>
 
@@ -20,6 +33,9 @@
   <Heading level="2" marginBottom={false}>
     An experiment in Fluid Visual Scaling for a Component Library type thingy.
   </Heading>
+  <div class="render">
+    <video autoplay loop class="video" src="example.mp4" />
+  </div>
   <Text>
     <p>
       Inspired by
